5b4d2fe1139aa7476199d2b369bf8036fc9a0594
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-12-01 23:30:51 +0200

    Add project files.

---
+++ functiiuzualematematica/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace functiiuzualematematica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        natural na = new natural();
+        natural nb = new natural();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void newline()
+        {
+            textBox2.Text += "\r\n";
+        }
+        public void deltextbox()
+        {
+            textBox2.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                na.a = Int16.Parse(textBox1.Text);
+                nb.a = Int16.Parse(textBox3.Text);
+
+            deltextbox();
+            textBox2.Text += "Nr A: " +  na.a.ToString(); newline();
+            textBox2.Text += "Nr B: " + nb.a.ToString(); newline();
+            textBox2.Text += "impar: " + na.eimpar().ToString(); newline();
+            textBox2.Text += "par: " + na.epar().ToString(); newline();
+            textBox2.Text += "prim: " + na.eprim().ToString(); newline();
+            textBox2.Text += "divide: " + na.edivizibil(nb).ToString(); newline();
+            textBox2.Text += "multiplu: " + na.emultiplu(nb).ToString(); newline();
+            textBox2.Text += "submultiplu: " + na.esubmultiplu(nb).ToString(); newline();
+            textBox2.Text += "egal: " + na.eegal(nb).ToString(); newline();
+            textBox2.Text += "diferit: " + na.ediferit(nb).ToString(); newline();
+            textBox2.Text += "mai mic: " + na.emaimic(nb).ToString(); newline();
+            textBox2.Text += "mai mare: " + na.emaimare(nb).ToString(); newline();
+            
+            }
+            catch { }
+        }
+    }
+}
---
+++ functiiuzualematematica/natural.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace functiiuzualematematica
+{
+    public class natural
+    {
+        public int a;
+
+        public bool epar()
+        {
+            if (a % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool eimpar()
+        {
+            if (a % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
+        }
+
+        public bool edivizibil(natural b)
+        {
+            if ( b.a % a== 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool emultiplu(natural b)
+        {
+            
+            natural x = new natural();
+            x.a = 0;
+            
+            x.a = a / b.a ;
+
+            if ((x.a * b.a) == a && a > b.a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool esubmultiplu(natural b)
+        {
+
+            if (b.a % a == 0 && a < b.a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool eprim()
+        {
+            for(int i = 2;i<=a/2;i++)
+            {
+                if (a % i==0)
+              {
+                return false;
+               }
+             }
+            return true;
+        }
+
+        public bool eegal(natural b)
+        {
+            if (b.a == a )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool ediferit(natural b)
+        {
+            if (b.a != a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool emaimare(natural b)
+        {
+            if (b.a < a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool emaimic(natural b)
+        {
+            if (b.a > a)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //de adaugat codul urmator
+        public natural cmmdc(natural b)
+        {
+            natural nrcmmdc = new natural();
+            nrcmmdc.a = 0;
+            // de adaugat codul aici
+            return nrcmmdc;
+        }
+
+        public natural cmmmc(natural b)
+        {
+            natural nrcmmmc = new natural();
+            nrcmmmc.a = 0;
+            // de adaugat codul aici
+            return nrcmmmc;
+        }
+
+        public void sumacifrelor() { }
+        public void sumacifrepare() { }
+        public void sumacifreimpare() { }
+
+        public void produscifrelor() { }
+        public void produscifrepare() { }
+        public void produscifreimpare() { }
+
+
+        public void contorcifrenumar() { }
+
+        public void emaimaresauegal() { }
+        public void emaimicsauegal() { }
+        public void evid() { }
+        public void enull() { }
+        public void einfinit() { }
+        public void ezero() { }
+        public void eintre() { }
+        //de adaugat alte functii specifice numerelor naturale
+        //public void () { }
+    }
+}
